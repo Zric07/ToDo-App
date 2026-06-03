4fb73b61f6f5fd522968512df2af752c18273291
Cedric Gruschwitz <cedric.gruschwitz@nhochdrei.de>
2026-06-03 10:40:25 +0200

    fix: register CapacitorSQLite plugin add: icon

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,13 +1,5 @@
 package com.example.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.community.database.sqlite.CapacitorSQLite;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(CapacitorSQLite.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
