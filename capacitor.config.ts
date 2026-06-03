8cb7b0f4a5cbc21580e8af9f7353cb69e718a89b
Cedric Gruschwitz <cedric.gruschwitz@nhochdrei.de>
2026-06-03 10:26:46 +0200

    fix: add CapacitorSQLite plugin config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'ToDo',
-  webDir: 'dist/frontend/browser'
+  webDir: 'dist/frontend/browser',
+  plugins: {
+    CapacitorSQLite: {
+      androidIsEncryption: false,
+    }
+  }
 };
 
 export default config;
