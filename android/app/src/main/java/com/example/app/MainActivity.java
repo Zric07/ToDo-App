441e3232d0688d220cc84ca3a7c9532978ca93da
Cedric Gruschwitz <cedric.gruschwitz@nhochdrei.de>
2026-06-03 10:15:57 +0200

    fix: register CapacitorSQLite plugin

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.jeep.plugin.capacitor.cdssutils.CapacitorSQLitePlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(CapacitorSQLitePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
