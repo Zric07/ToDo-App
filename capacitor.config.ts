835dcfdd828edb26e6b2fd0ae29a0ffb72962ea1
Cedric Gruschwitz <cedric.gruschwitz@nhochdrei.de>
2026-06-05 09:36:36 +0200

    change: name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'ToDo',
+  appName: 'Grind',
   webDir: 'dist/frontend/browser',
   plugins: {
     CapacitorSQLite: {
